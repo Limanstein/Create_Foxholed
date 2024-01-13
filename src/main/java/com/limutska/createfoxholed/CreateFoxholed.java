656e98747b9b6ba4686a2679ff13633e528a38b9
Limutska <mospt.the.gamer@gmail.com>
2024-01-13 03:05:11 +0000

    Change 1

--- src/main/java/com/limutska/createfoxholed/CreateFoxholed.java
+++ src/main/java/com/limutska/createfoxholed/CreateFoxholed.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+// a cool coment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CreateFoxholed.MOD_ID)
 public class CreateFoxholed{
